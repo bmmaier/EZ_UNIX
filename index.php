79016ea4b923c62515019ef708fe76ec0debc857
bmmaier <158106263+bmmaier@users.noreply.github.com>
2024-04-16 11:38:35 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EZ UNIX Guide</title>
+    <link rel="stylesheet" href="home.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+    <?php include "navigation.php"; ?>
+
+    <div class="main_wrapper">
+        <div class="hero_section">
+            <div class="hero_text">
+                <h1>EZ UNIX Guide</h1>
+            </div>
+        </div>
+
+        <div class="content_sections_wrapper">
+            <div class="places_section">
+                
+                <div class="place_cards_holder">
+                    <div class="place_card">
+                        <a href="beginner.html"><h3>BEGINNER</h3></a> 
+                    </div>
+                    <div class="place_card">
+                        <a href="intermediate.html"><h3>INTERMEDIATE</h3></a> 
+                    </div>
+                    <div class="place_card">
+                        <a href="advanced.html"><h3>ADVANCED</h3></a> 
+                    </div>
+                </div>
+            </div>
+
+            <div class="additional_place_cards_holder">
+                <div class="additional_place_card">
+                    <a href="multimedia.html"><h3>MULTIMEDIA</h3></a>
+                </div>
+                <div class="additional_place_card">
+                    <a href="test.html"><h3>TEST YOUR KNOWLEDGE</h3></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
---
+++ navigation.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+    <div class="home_link">
+        <img id="ez_logo" src="unix.png" alt="EZ Logo">
+    </div>
+
+    <div class="other_links">
+        <a href="home.html">HOME</a>
+        <a href="start.html">GETTING STARTED</a>
+        <a href="troubleshooting.html">TROUBLESHOOTING</a>
+        <a href="community.html">COMMUNITY</a>
+    </div>
+</nav>
